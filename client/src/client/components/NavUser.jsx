5592f4fd65b83c6c6adfa1ee49eb03379eab2cf5
hovietvinh <hovietvinh29032004@gmail.com>
2024-10-17 19:10:13 +0700

    res

--- client/src/client/components/NavUser.jsx
+++ client/src/client/components/NavUser.jsx
@@ -6,23 +6,23 @@ import {UserOutlined,UnorderedListOutlined,HomeOutlined} from "@ant-design/icons
 function NavUser() {
     return (
         <>
-            <nav className='flex mt-8 gap-4 w-full justify-center mb-8'>
+            <nav className='flex mt-8 gap-2 sm:gap-4 w-full justify-center mb-8'>
                 <NavLink  to="/user/profile" 
                     className={({ isActive }) =>
-                    `py-2 inline-flex gap-1 transition duration-300 px-4 rounded-full ${isActive ? "rounded-full bg-[#f5385d] text-white " : 'bg-gray-200'}`
+                    `text-[12px] sm:text[18px] py-2 inline-flex gap-1 transition duration-300 px-4 rounded-full ${isActive ? "rounded-full bg-[#f5385d] text-white " : 'bg-gray-200'}`
                     }>
                     <UserOutlined />
                     My profile
                     </NavLink>
                 <NavLink to="/user/bookings" 
                     className={({ isActive }) =>
-                    `py-2 px-4 inline-flex gap-1   transition duration-300 rounded-full ${isActive ? "rounded-full bg-[#f5385d] text-white " : 'bg-gray-200'}`
+                    `text-[12px] sm:text[18px] py-2 px-4 inline-flex gap-1   transition duration-300 rounded-full ${isActive ? "rounded-full bg-[#f5385d] text-white " : 'bg-gray-200'}`
                     }>
                     <UnorderedListOutlined />
                     My bookings</NavLink>
                 <NavLink to="/user/places" 
                 className={({ isActive }) =>
-                `py-2 px-4 inline-flex gap-1   transition duration-300 rounded-full ${isActive ? " bg-[#f5385d] text-white " : 'bg-gray-200'}`
+                `text-[12px] sm:text[18px] py-2 px-4 inline-flex gap-1   transition duration-300 rounded-full ${isActive ? " bg-[#f5385d] text-white " : 'bg-gray-200'}`
                 }>
                     <HomeOutlined />
                     My accommodations</NavLink>
