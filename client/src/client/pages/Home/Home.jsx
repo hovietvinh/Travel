e9bee49d15aa21b357fd1120181d606dfd3fa95d
hovietvinh <hovietvinh29032004@gmail.com>
2024-10-17 18:20:08 +0700

    spin

--- client/src/client/pages/Home/Home.jsx
+++ client/src/client/pages/Home/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
         <>
             {places.length==0 &&(
                 <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-                    <Spin spinning={data.length === 0} tip="Waiting...">
+                    <Spin spinning={places.length === 0} tip="Waiting...">
                     
                     </Spin>
                 </div>
